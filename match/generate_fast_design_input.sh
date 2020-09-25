d7c3647311781a4357213332b1272898ef7bbd01
Zhuofan Shen <shenzhuofan01@gmail.com>
2020-09-25 19:17:45 -0400

    Update generate_fast_design_input.sh

--- match/generate_fast_design_input.sh
+++ match/generate_fast_design_input.sh
@@ -14,37 +14,23 @@ then
     homomeric="--homomeric"
 fi
 
-scaffold=${positions##*/}
-scaffold=${positions##*\\}
-and="-and"
-or="-or"
+scaffold=${position_files##*/}
 for scaffold_linker in `ls`
 do
     if [[ ${scaffold_linker} == ${scaffold}* ]]
     then
-        linker=${scaffold_linker##*_}
-        if [[ ${linker} == *-*-* ]]
-        then
-            or=${or}" "${scaffold_linker}
-        else
-            and=${and}" "${scaffold_linker}
-        fi
+        directories=${directories}" "${scaffold_linker}
         cd ${scaffold_linker}
         mkdir match
-        mv ${scaffold}*/* match
-        rm -rf ${scaffold}*
+        cp ${scaffold}*/* match
+        # rm -rf ${scaffold}*
         # make sure that your python3 or anaconda3 has the pymol module installed.
         # conda config --set auto_activate_base false
-        # eval "$(~/anaconda3/bin/conda shell.bash hook)"
+        # eval "$(/home/zs251/anaconda3/bin/conda shell.bash hook)"
         python ../../scripts/generate_fast_design_input.py match ${homomeric}
         # conda deactivate
         cd ..
     fi
 done
 
-if [[ ${or} == "-or" ]]
-then
-    ../scripts/find_match_intersection.py ${and}
-else
-    ../scripts/find_match_intersection.py ${and} ${or}
-fi
+python ../scripts/find_match_intersection.py ${directories}
