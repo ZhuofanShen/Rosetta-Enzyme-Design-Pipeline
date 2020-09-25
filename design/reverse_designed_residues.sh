8b01ee83808934d598ded14e128abbffb898ad39
Zhuofan Shen <shenzhuofan01@gmail.com>
2020-09-25 00:41:45 -0400

    Update reverse_designed_residues.sh

--- design/reverse_designed_residues.sh
+++ design/reverse_designed_residues.sh
@@ -2,14 +2,14 @@
 while (( $# > 1 ))
 do
     case $1 in
-        -pdb) reference_pdb="$2";;
-        -scaf) scaffold="$2";; # i.e. CPG2-AB
+        -pos) position_files="$2";;
         -linker) linker="$2";; # i.e. ../pAaF/pAaF-product
-        -params) params="$2";; #optional, only for ligands of the protein
         -symm) symmetry="$2";; #optional
         -nbh) neighborhood="$2";; #optional
         -n) decoys="$2";; #optional
-        -mem) memory="$2";; #optional
+        -mem) memory="$2";;
+        -ligand) ligand="$2";; #optional, only for ligands or cofactors of the protein, not matching residues
+        -get_best) get_the_best_design="$2";; # Optional. If this flag is given, get_the_best_design.py will be called.
         *) break;
     esac
     shift 2
@@ -32,25 +32,56 @@ else
     decoys="-decoys "${decoys}
 fi
 
-if ! [ -z "${memory}" ]
+# if ! [ -z "${memory}" ]
+# then
+#     memory="--mem ${memory}"
+# fi
+
+linker_res_name=$(ls ${linker}/*_design.params)
+linker_res_name=${linker_res_name##*/}
+linker_res_name=${linker_res_name%_design.params}
+
+IFS=','
+if ! [ -z "${ligand}" ]
 then
-    memory="--mem ${memory}"
+    params_files=""
+    for params_file in ${ligand[@]}
+    do
+        params_files=${ligand}" ../"${params_file}
+    done
 fi
 
-pdb_name=
-IFS=','
-if ! [ -z "${params}" ]
+path_to_protein=${position_files%/*}
+scaffold=${position_files##*/}
+substrate=${linker##*/}
+
+if ! [ -z "${get_the_best_design}" ]
 then
-    ligand=""
-    for params_file in ${params[@]}
+    protein=${scaffold%-*}
+    reference_pdb=$(ls ${path_to_protein}/${protein}*.pdb)
+
+    if ! [ -z "${symmetry}" ]
+    then
+        homomeric="-homo"
+    fi
+
+    srun --job-name get_the_best_design --partition p_sdk94_1 --time 1:00:00 \
+        python ../scripts/get_the_best_design.py ${scaffold}_${substrate} -ref ${reference_pdb} \
+        -params ${linker}/${linker_res_name}_design.params ${linker}/CYX.params \
+        ${linker}/TYZ.params ${params_files} -linker ${linker_res_name} ${homomeric}
+fi
+
+if ! [ -z "${ligand}" ]
+then
+    params_files=""
+    for params_file in ${ligand[@]}
     do
-        ligand=${ligand}" ../../../"${params_file}
+        params_files=${ligand}" ../../"${params_file}
     done
 fi
 IFS='
 '
 
-substrate=${linker##*/}
 cd ${scaffold}_${substrate}
 
 for variant in `ls`
@@ -58,8 +89,6 @@ do
     if [[ ${variant} == X*Z* ]]
     then
         cd ${variant}
-        rotlib=$(ls *.rotlib.pdb)
-        linker_res_name=${rotlib%%.rotlib.pdb*}
         design=$(ls ${scaffold}_*.pdb)
         design=${design:0:-4}
         design=${design##${scaffold}_}
@@ -69,7 +98,7 @@ do
         '
         for i in ${!design[@]}
         do
-            if [[ ${design[${i}]} == *X or ${design[${i}]} == *Z ]]
+            if [[ ${design[${i}]} == *X ]] || [[ ${design[${i}]} == *Z ]]
             then
                 unset 'design[${i}]'
             fi
@@ -93,9 +122,9 @@ do
                 fi
                 mkdir revert_${reverted_point_mutation}
                 cd revert_${reverted_point_mutation}
-                slurmit.py --job ${variant} ${memory} --command "python3 ../../../../scripts/fast_design_matches.py \
+                slurmit.py --job ${variant} --mem ${memory} --command "python ../../../../scripts/fast_design.py \
                     ../${variant}.pdb -sf beta_nov16_cst -params ../../../${linker}/${linker_res_name}_design.params \
-                    ../../../${linker}/CYX.params ../../../${linker}/TYZ.params ${symmetry} ${duplicated_chains}\
+                    ../../../${linker}/CYX.params ../../../${linker}/TYZ.params ${params_files} ${symmetry} \
                     -enzdescst ../../../${linker}/${substrate}_design.cst -rmsd True -nataa True \
                     ${point_mutations} ${neighborhood} ${decoys};"
                 sleep 0.1
