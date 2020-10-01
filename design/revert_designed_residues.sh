bd452b1f5333c6339440a34fcc26e43102094c97
Zhuofan Shen <shenzhuofan01@gmail.com>
2020-10-01 17:16:14 -0400

    Update revert_designed_residues.sh

--- design/revert_designed_residues.sh
+++ design/revert_designed_residues.sh
@@ -2,15 +2,15 @@
 while (( $# > 1 ))
 do
     case $1 in
-        -pos) position_files="$2";;
+        -pos) position_files="$2";; # i.e. ../CPG2/CPG2-AB
         -linker) linker="$2";; # i.e. ../pAaF/pAaF-product
-        -symm) symmetry="$2";; #optional
+        -symm) symmetry="$2";; # optional
         -dup) duplicate_match="$2";; # optional
-        -nbh) neighborhood="$2";; #optional
-        -n) decoys="$2";; #optional
+        -nbh) neighborhood="$2";; # optional
+        -n) decoys="$2";; # optional
         -mem) memory="$2";;
-        -ligand) ligand="$2";; #optional, only for ligands or cofactors of the protein, not matching residues
-        -get_best) get_the_best_design="$2";; # Optional. If this flag is given, get_the_best_design.py will be called.
+        -ligand) ligand="$2";; # Optional, only for ligands or cofactors of the protein, not for matching residues or ligands.
+        -not_get_best) not_get_the_best_design="$2";; # Optional. If this flag is given, get_the_best_design.py will not be called.
         *) break;
     esac
     shift 2
@@ -45,46 +45,58 @@ fi
 #     memory="--mem ${memory}"
 # fi
 
-linker_res_name=$(ls ${linker}/*_design.params)
+linker_res_name=$(ls ${linker}/*_ligand.params)
 linker_res_name=${linker_res_name##*/}
-linker_res_name=${linker_res_name%_design.params}
+linker_res_name=${linker_res_name%_ligand.params}
+
+params_files="-params"
+for params_file in `ls ${linker}/*.params`
+do
+    params_files=${params_files}" ../"${params_file}
+done
 
 IFS=','
 if ! [ -z "${ligand}" ]
 then
-    params_files=""
     for params_file in ${ligand[@]}
     do
-        params_files=${ligand}" ../"${params_file}
+        params_files=${params_files}" ../"${params_file}
     done
 fi
+IFS='
+'
 
 path_to_protein=${position_files%/*}
 scaffold=${position_files##*/}
 substrate=${linker##*/}
 
-if ! [ -z "${get_the_best_design}" ]
+if [ -z "${not_get_the_best_design}" ]
 then
     protein=${scaffold%-*}
     reference_pdb=$(ls ${path_to_protein}/${protein}*.pdb)
 
     if ! [ -z "${symmetry}" ]
     then
-        homomeric="-homo"
+        ignore_symmetric_mutations="-symm"
     fi
 
     srun --job-name get_the_best_design --partition p_sdk94_1 --time 1:00:00 \
         python ../scripts/get_the_best_design.py ${scaffold}_${substrate} -ref ${reference_pdb} \
-        -params ${linker}/${linker_res_name}_design.params ${linker}/CYX.params \
-        ${linker}/TYZ.params ${params_files} -linker ${linker_res_name} ${homomeric}
+        ${params_files} -linker ${linker_res_name} ${ignore_symmetric_mutations}
 fi
 
+params_files="-params"
+for params_file in `ls ${linker}/*.params`
+do
+    params_files=${params_files}" ../../../"${params_file}
+done
+
+IFS=','
 if ! [ -z "${ligand}" ]
 then
-    params_files=""
     for params_file in ${ligand[@]}
     do
-        params_files=${ligand}" ../../"${params_file}
+        params_files=${params_files}" ../../../"${params_file}
     done
 fi
 IFS='
@@ -99,26 +111,26 @@ do
         cd ${variant}
         design=$(ls ${scaffold}_*.pdb)
         design=${design:0:-4}
-        design=${design##${scaffold}_}
+        point_mutations=${design##${scaffold}_}
         IFS='_'
-        design=(${design[@]})
+        point_mutations=(${point_mutations[@]})
         IFS='
         '
-        for i in ${!design[@]}
+        for i in ${!point_mutations[@]}
         do
-            if [[ ${design[${i}]} == *X ]] || [[ ${design[${i}]} == *Z ]]
+            if [[ ${point_mutations[${i}]} == *X ]] || [[ ${point_mutations[${i}]} == *Z ]]
             then
-                unset 'design[${i}]'
+                unset 'point_mutations[${i}]'
             fi
         done
-        if [[ ${#design[@]} > 0 ]]
+        if [[ ${#point_mutations[@]} > 0 ]]
         then
-            for reverted_point_mutation in ${design[@]}
+            for reverted_point_mutation in ${point_mutations[@]}
             do
-                if [[ ${#design[@]} > 1 ]]
+                if [[ ${#point_mutations[@]} > 1 ]]
                 then
                     point_mutations="-muts"
-                    for point_mutation in ${design[@]}
+                    for point_mutation in ${point_mutations[@]}
                     do
                         if [[ ${point_mutation} != ${reverted_point_mutation} ]]
                         then
@@ -131,11 +143,10 @@ do
                 mkdir revert_${reverted_point_mutation}
                 cd revert_${reverted_point_mutation}
                 slurmit.py --job ${variant} --mem ${memory} --command "python ../../../../scripts/fast_design.py \
-                    ../${variant}.pdb -sf ref2015_cst -params ../../../${linker}/${linker_res_name}_design.params \
-                    ../../../${linker}/CYX.params ../../../${linker}/TYZ.params ${params_files} ${symmetry} \
-                    -enzdescst ../../../${linker}/${substrate}${enzdes_cst_suffix} -rmsd True -nataa True \
-                    ${point_mutations} ${neighborhood} ${decoys};"
-                sleep 0.1
+                    ../${variant}.pdb -sf ref2015_cst ${params_files} ${symmetry} \
+                    -enzdescst ../../../${linker}/${substrate}${enzdes_cst_suffix} \
+                    ${point_mutations} ${neighborhood} -rmsd True ${decoys};"
+                sleep 0.05
                 cd ..
             done
         fi
