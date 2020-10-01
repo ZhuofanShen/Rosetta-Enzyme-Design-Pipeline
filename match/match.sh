29bc7b06164b9bdbfdc7707d0c5c252d3f2ec462
Zhuofan Shen <shenzhuofan01@gmail.com>
2020-10-01 17:19:43 -0400

    Update match.sh

--- match/match.sh
+++ match/match.sh
@@ -2,10 +2,10 @@
 while (( $# > 1 ))
 do
     case $1 in
-        -pos) position_files="$2";;
-        -linker) linker="$2";;
+        -pos) position_files="$2";; # i.e. ../CPG2/CPG2-AB
+        -linker) linker="$2";; # i.e. ../pAaF/pAaF-product
         -mem) memory="$2";;
-        -ligand) ligand="$2";; #optional, only for ligands or cofactors of the protein, not matching residues
+        -ligand) ligand="$2";; # Optional, only for ligands or cofactors of the protein, not for matching residues or ligands.
         *) break;
     esac
     shift 2
@@ -31,7 +31,7 @@ IFS='
 path_to_protein=${position_files%/*}
 scaffold=${position_files##*/}
 protein=${scaffold%-*}
-pdb=$(ls ${path_to_protein}/${protein}*.pdb)
+reference_pdb=$(ls ${path_to_protein}/${protein}*.pdb)
 
 substrate=${linker##*/}
 mkdir ${scaffold}_${substrate}
@@ -43,9 +43,9 @@ do
     scaffold_part=${scaffold_part%.pos}
     mkdir ${scaffold_part}
     cd ${scaffold_part}
-    slurmit.py --job ${variant} --mem ${memory} --command "~/Rosetta/main/source/bin/match.linuxgccrelease \
+    slurmit.py --job ${scaffold_part} --mem ${memory} --command "~/Rosetta/main/source/bin/match.linuxgccrelease \
         @../../../scripts/general_match.flags @../../${linker}/subs.flags ${params_files} \
-        -match:scaffold_active_site_residues_for_geomcsts ../${pos} -s ../../${pdb}"
+        -match:scaffold_active_site_residues_for_geomcsts ../${pos} -s ../../${reference_pdb}"
     cd ..
     sleep 0.1
 done
