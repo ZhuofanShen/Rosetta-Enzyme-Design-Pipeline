fb927fed7e31e4d0956a2c91e4fd964536eb3648
Zhuofan Shen <shenzhuofan01@gmail.com>
2020-10-02 17:07:27 -0400

    Update sftp_best_decoys.py

--- design/sftp_best_decoys.py
+++ design/sftp_best_decoys.py
@@ -10,10 +10,10 @@ parser.add_argument('-u', '--username', type=str, required=True, help='NetID')
 parser.add_argument('-p', '--password', type=str, required=True)
 args = parser.parse_args()
 
-directory = args.directory[args.directory.find('/') + 1:]
+directory = args.directory[args.directory.rfind('/') + 1:]
 os.mkdir(directory)
 
-workbook = xlrd.open_workbook('scores.xls')
+workbook = xlrd.open_workbook(directory + '.xls')
 score_sheet = workbook.sheet_by_index(0)
 num_sheet = workbook.sheet_by_index(1)
 
@@ -28,20 +28,23 @@ for row in range(0, num_sheet.nrows):
     match = num_sheet.cell_value(row, 0)
     # design
     num = num_sheet.cell_value(row, 1)
-    stdin, stdout, stderr = ssh_client.exec_command('ls ' + args.directory + '/' + match + '/design/*_' + num + '.pdb')
+    stdin, stdout, stderr = ssh_client.exec_command('ls ' + args.directory + '/' + match + '/design/*_' + str(int(num)) + '.pdb')
     best_decoy_full_name = stdout.readlines()[0][:-1]
     sftp_client.get(best_decoy_full_name, directory + '/' + design + '/' + design + '.pdb')
     col = 2
     # revert
-    reverted_design = num_sheet.cell_value(row, col)
-    col += 1
-    while reverted_design:
-        mum = num_sheet.cell_value(row, col)
+    while True:
+        try:
+            reverted_design = num_sheet.cell_value(row, col)
+            reverted_design_pdb_index = score_sheet.cell_value(row, col)
+        except:
+            break
         col += 1
-        stdin, stdout, stderr = ssh_client.exec_command('ls ' + args.directory + '/' + match + '/' + reverted_design + '/*_' + num + '.pdb')
+        num = num_sheet.cell_value(row, col)
+        col += 1
+        stdin, stdout, stderr = ssh_client.exec_command('ls ' + args.directory + '/' + match + '/' + reverted_design + '/*_' + str(int(num)) + '.pdb')
         best_decoy_full_name = stdout.readlines()[0][:-1]
-        sftp_client.get(best_decoy_full_name, directory + '/' + design + '/' + reverted_design + '.pdb')
-        reverted_design = num_sheet.cell_value(row, col)
-        col += 1
+        sftp_client.get(best_decoy_full_name, directory + '/' + design + '/revert_' + reverted_design_pdb_index + '.pdb')
 sftp_client.close()
 ssh_client.close()
+os.rename(directory + '.xls', directory + '/' + directory + '.xls')
