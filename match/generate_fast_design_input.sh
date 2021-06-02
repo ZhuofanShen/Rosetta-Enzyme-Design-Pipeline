4de05b69e2ff53386012d43a52f76eb0ff3ebb3b
Zhuofan Shen <shenzhuofan01@gmail.com>
2021-06-02 17:49:11 -0400

    Update generate_fast_design_input.sh

--- match/generate_fast_design_input.sh
+++ match/generate_fast_design_input.sh
@@ -4,6 +4,7 @@ do
     case $1 in
         -pos) position_files="$2";;
         -dup) duplicate_match="$2";; # optional
+        -sym) symmetry="$2";; # optional
         *) break;
     esac
     shift 2
@@ -14,10 +15,15 @@ then
     duplicate_match="--duplicate_match"
 fi
 
+if ! [ -z "${symmetry}" ]
+then
+    symmetry="--symmetry"
+fi
+
 scaffold=${position_files##*/}
 for scaffold_linker in `ls`
 do
-    if [[ ${scaffold_linker} == ${scaffold}* ]]
+    if [[ ${scaffold_linker} == ${scaffold}_* ]]
     then
         directories=${directories}" "${scaffold_linker}
         cd ${scaffold_linker}
@@ -25,7 +31,7 @@ do
         do
             if [[ ${pos_part} == ${scaffold}_* ]]
             then
-                python ../../scripts/generate_fast_design_input.py ${pos_part} ${duplicate_match}
+                python ../../scripts/generate_fast_design_input.py ${pos_part} ${duplicate_match} ${symmetry}
             fi
         done
         cd ..
