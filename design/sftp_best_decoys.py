caba2d48b4a3e8ff4b0081cb4d468fc15d0d5f74
Zhuofan Shen <shenzhuofan01@gmail.com>
2021-06-02 18:34:52 -0400

    Update sftp_best_decoys.py

--- design/sftp_best_decoys.py
+++ design/sftp_best_decoys.py
@@ -8,43 +8,53 @@ parser = argparse.ArgumentParser()
 parser.add_argument('directory', type=str, help='/home/NetID/protein_stapling/CPG2_pAaF/CPG2-AB_pAaF-product')
 parser.add_argument('-u', '--username', type=str, required=True, help='NetID')
 parser.add_argument('-p', '--password', type=str, required=True)
+parser.add_argument('-s', '--step', type=int, choices=[0, 1, 2], required=True)
 args = parser.parse_args()
 
-directory = args.directory[args.directory.rfind('/') + 1:]
-os.mkdir(directory)
+scaf_subs = args.directory[args.directory.rfind('/') + 1:]
 
-workbook = xlrd.open_workbook(directory + '.xls')
-score_sheet = workbook.sheet_by_index(0)
-num_sheet = workbook.sheet_by_index(1)
+workbook = xlrd.open_workbook(scaf_subs + '.xls')
+directory_sheet = workbook.sheet_by_index(0)
+design_sheet = workbook.sheet_by_index(args.step)
+
+if args.step == 1:
+    directory = 'design'
+elif args.step == 2:
+    directory = 'manual_design'
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname='amarel.hpc.rutgers.edu', port=22, username=args.username, password=args.password)
 sftp_client = ssh_client.open_sftp()
 
-for row in range(0, num_sheet.nrows):
-    design = score_sheet.cell_value(row, 0)
-    os.mkdir(directory + '/' + design)
-    match = num_sheet.cell_value(row, 0)
-    # design
-    num = num_sheet.cell_value(row, 1)
-    stdin, stdout, stderr = ssh_client.exec_command('ls ' + args.directory + '/' + match + '/design/*_' + str(int(num)) + '.pdb')
-    best_decoy_full_name = stdout.readlines()[0][:-1]
-    sftp_client.get(best_decoy_full_name, directory + '/' + design + '/' + design + '.pdb')
-    col = 2
-    # revert
-    while True:
+for row in range(0, directory_sheet.nrows):
+    variant = directory_sheet.cell_value(row, 0)
+    print(variant)
+    if not os.path.isdir(variant):
+        os.mkdir(variant)
+    if args.step == 0:
+        sftp_client.get(args.directory + '/' + variant + '/' + variant + '.pdb', variant + '/' + variant + '.pdb')
         try:
-            reverted_design = num_sheet.cell_value(row, col)
-            reverted_design_pdb_index = score_sheet.cell_value(row, col)
+            sftp_client.get(args.directory + '/' + variant + '/' + variant + '-ligand.pdb', variant + '/' + variant + '-ligand.pdb')
         except:
-            break
-        col += 1
-        num = num_sheet.cell_value(row, col)
-        col += 1
-        stdin, stdout, stderr = ssh_client.exec_command('ls ' + args.directory + '/' + match + '/' + reverted_design + '/*_' + str(int(num)) + '.pdb')
+            pass
+    else:
+        try:
+            design = design_sheet.cell_value(row, 0)
+            print(design)
+        except IndexError:
+            print(variant + ' has not been designed yet.')
+            continue
+        num = design_sheet.cell_value(row, 2)
+        if num == '':
+            print(variant + ' has not been designed yet.')
+            continue
+        print('(' + str(row) + ', 1): ' + str(int(num)))
+        stdin, stdout, stderr = ssh_client.exec_command('ls ' + args.directory + '/' + variant + '/' + directory + '/*_' + str(int(num)) + '.pdb')
         best_decoy_full_name = stdout.readlines()[0][:-1]
-        sftp_client.get(best_decoy_full_name, directory + '/' + design + '/revert_' + reverted_design_pdb_index + '.pdb')
+        sftp_client.get(best_decoy_full_name, variant + '/' + variant + '_' + directory + '.pdb')
+        if args.step == 1:
+            sftp_client.get(args.directory + '/' + variant + '/pymol.txt', variant + '/pymol.txt')
+
 sftp_client.close()
 ssh_client.close()
-os.rename(directory + '.xls', directory + '/' + directory + '.xls')
