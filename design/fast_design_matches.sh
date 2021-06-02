deb3549b99311dc50efba13b2a9dde63f4026655
Zhuofan Shen <shenzhuofan01@gmail.com>
2021-06-02 18:12:26 -0400

    Update fast_design_matches.sh

--- design/fast_design_matches.sh
+++ design/fast_design_matches.sh
@@ -4,25 +4,18 @@ do
     case $1 in
         -pos) position_files="$2";; # i.e. ../CPG2/CPG2-AB
         -linker) linker="$2";; # i.e. ../pAaF/pAaF-product
-        -symm) symmetry="$2";; #optional, symmetric file.
-        -dup) duplicate_match="$2";; # optional
+        -cst_suffix) cst_suffix="$2";; # optional
         -nbh) neighborhood="$2";; #optional
         -n) decoys="$2";; #optional
         -mem) memory="$2";;
-        -ligand) ligand="$2";; # Optional, only for ligands or cofactors of the protein, not for matching residues or ligands.
         *) break;
     esac
     shift 2
 done
 
-if ! [ -z "${symmetry}" ]
+if ! [ -z "${cst_suffix}" ]
 then
-    symmetry="-symm ../../../"${symmetry}
-fi
-
-if ! [ -z "${duplicate_match}" ]
-then
-    enzdes_cst_suffix=_symm.cst
+    enzdes_cst_suffix=_${cst_suffix}.cst
 else
     enzdes_cst_suffix=.cst
 fi
@@ -34,9 +27,9 @@ fi
 
 if [ -z "${decoys}" ]
 then
-    decoys="-decoys 50"
+    decoys="-n 50"
 else
-    decoys="-decoys "${decoys}
+    decoys="-n "${decoys}
 fi
 
 # if ! [ -z "${memory}" ]
@@ -49,17 +42,17 @@ for params_file in `ls ${linker}/*.params`
 do
     params_files=${params_files}" ../../../"${params_file}
 done
+path_to_protein=${position_files%/*}
+for params_file in `ls ${path_to_protein}/*.params`
+do
+    params_files=${params_files}" ../../../"${params_file}
+done
 
-IFS=','
-if ! [ -z "${ligand}" ]
-then
-    for params_file in ${ligand[@]}
-    do
-        params_files=${params_files}" ../../../"${params_file}
-    done
-fi
-IFS='
-'
+for symmetry in `ls ${path_to_protein}/*.symm`
+do
+    symmetry="-symm ../../../"${symmetry}
+    break
+done
 
 scaffold=${position_files##*/}
 substrate=${linker##*/}
@@ -67,15 +60,17 @@ cd ${scaffold}_${substrate}
 
 for variant in `ls`
 do
-    if [[ ${variant} == X*Z* ]]
+    if [[ ${variant} == X*Z* ]] && [[ ${variant} != *_deprecated ]]
     then
         cd ${variant}
         mkdir design
         cd design
-        slurmit.py --job ${variant} --mem ${memory} --command "python ../../../../scripts/fast_design.py \
-            ../${variant}.pdb -sf ref2015_cst ${params_files} ${symmetry} \
-            -enzdescst ../../../${linker}/${substrate}${enzdes_cst_suffix} \
-            --enzdes -nataa True ${neighborhood} -rmsd True ${decoys};"
+        slurmit.py --job ${variant} --mem ${memory} --command \
+            "python ../../../../scripts/fast_design.py ../${variant}.pdb \
+            ${params_files} ${symmetry} -sf ref2015_cst \
+            --score_terms fa_intra_rep_nonprotein:0.545 fa_intra_atr_nonprotein:1 \
+            -enzdes_cst ../../../${linker}/${substrate}${enzdes_cst_suffix} \
+            -enzdes -no_cys -nataa 1.5 ${neighborhood} ${decoys};"
         cd ../..
         sleep 0.05
     fi
