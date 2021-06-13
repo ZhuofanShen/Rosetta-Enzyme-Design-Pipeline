586f5385b71ad86ca714d1787a5d4ae7926f29f9
Zhuofan Shen <shenzhuofan01@gmail.com>
2021-06-13 16:10:41 -0400

    Delete plot_res_score.sh

--- design/plot_res_score.sh
+++
@@ -1,5 +0,0 @@
-python ../../../scripts-design/plot_res_score.py -pdbs X7Z312_manual_design.pdb,X7Z312_design.pdb -diff yes
-
-python ../../../scripts-design/plot_res_score.py -pdbs X7Z312_manual_design.pdb,../1a4l_relaxed.pdb -diff yes
-
-python ../../../scripts-design/plot_res_score.py -pdbs ../1a4l_relaxed.pdb,X7Z312_design.pdb -diff yes
