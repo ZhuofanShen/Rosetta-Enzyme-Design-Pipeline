7e4a7ebacd49015cf1c7e015cd44540d261c28f7
Zhuofan Shen <shenzhuofan01@gmail.com>
2025-05-11 01:19:56 -0400

    Update run_design.sh

--- hemoprotein_biocatalysts_design/scripts_fast_design/run_design.sh
+++ hemoprotein_biocatalysts_design/scripts_fast_design/run_design.sh
@@ -1,1 +1,1 @@
-for path in `ls HEM/*/cyclopropanation_EDA_styrene/*/*/*rot*/design.sh`; do cd ${path%/*}; bash ${path##*/}; sleep 0.01; cd ../../../../../..; done
+for path in `ls HEM/*/cyclopropanation_styrene_EDA/*/*/*rot*/design.sh`; do cd ${path%/*}; bash ${path##*/}; sleep 0.01; cd ../../../../../..; done
